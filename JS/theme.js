a70225843c1b3eefd0c81a3e361656543bd9c5cf
Doha <dohaamed22@gmail.com>
2024-08-29 19:33:26 +0300

    dark theme for home page

--- JS/theme.js
+++ JS/theme.js
@@ -1,11 +1,13 @@
 function changeTheme() {
   document.querySelector("header").classList.toggle("dark-header");
+  document.querySelectorAll(".card").forEach((item) => {
+    item.classList.toggle("dark-header");
+  });
+  document.querySelector("body").classList.toggle("dark-bckgnd");
+
   document.querySelector("select").classList.toggle("dark-header");
-  document.querySelector("option").classList.toggle("dark-header");
+  // document.querySelector("option").classList.toggle("dark-header");
   document.querySelector(".search").classList.toggle("dark-header");
   document.querySelector(".search-input").classList.toggle("dark-header");
-
-  document.querySelector("html").classList.toggle("dark-bckgnd");
-  document.querySelector(".search-bar").classList.toggle("dark-bckgnd");
   document.querySelector(".btn-mode").classList.toggle("dark-button");
 }
