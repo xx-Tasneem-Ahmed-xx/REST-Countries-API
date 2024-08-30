3420f7aef5936d8bdadbc5a009737f7b5de9de14
Doha <dohaamed22@gmail.com>
2024-08-30 16:46:48 +0300

    consistenet theme

--- JS/theme.js
+++ JS/theme.js
@@ -1,22 +1,72 @@
-function changeTheme() {
-  document.querySelector("header").classList.toggle("dark-header");
-  document.querySelectorAll(".card").forEach((item) => {
-    item.classList.toggle("dark-header");
-  });
-  document.querySelector("body").classList.toggle("dark-bckgnd");
+function applyTheme() {
+  if (localStorage.getItem("theme") == "dark") darkTheme();
+  else lightTheme();
+}
 
-  document.querySelector("select").classList.toggle("dark-header");
-  document.querySelector(".search").classList.toggle("dark-header");
-  document.querySelector(".search-input").classList.toggle("dark-header");
-  document.querySelector(".btn-mode").classList.toggle("dark-button");
+function applyInfoTheme() {
+  if (localStorage.getItem("theme") == "dark") darkInfo();
+  else lightInfo();
+}
+
+function changeTheme() {
+  let theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    localStorage.setItem("theme", "light");
+    lightTheme();
+  } else {
+    localStorage.setItem("theme", "dark");
+    darkTheme();
+  }
 }
 
 function infoTheme() {
-  document.querySelector("header").classList.toggle("dark-header");
-  document.querySelector("body").classList.toggle("dark-bckgnd");
-  document.querySelector(".btn-mode").classList.toggle("dark-button");
-  document.querySelector(".back-btn").classList.toggle("dark-button");
+  let theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    localStorage.setItem("theme", "light");
+    lightInfo();
+  } else {
+    localStorage.setItem("theme", "dark");
+    darkInfo();
+  }
+}
+
+function darkTheme() {
+  document.querySelector("header").classList.add("dark-header");
+  document.querySelectorAll(".card").forEach((item) => {
+    item.classList.add("dark-header");
+  });
+  document.querySelector("body").classList.add("dark-bckgnd");
+  document.querySelector("select").classList.add("dark-header");
+  document.querySelector(".search").classList.add("dark-header");
+  document.querySelector(".search-input").classList.add("dark-header");
+  document.querySelector(".btn-mode").classList.add("dark-button");
+}
+function lightTheme() {
+  document.querySelector("header").classList.remove("dark-header");
+  document.querySelectorAll(".card").forEach((item) => {
+    item.classList.remove("dark-header");
+  });
+  document.querySelector("body").classList.remove("dark-bckgnd");
+  document.querySelector("select").classList.remove("dark-header");
+  document.querySelector(".search").classList.remove("dark-header");
+  document.querySelector(".search-input").classList.remove("dark-header");
+  document.querySelector(".btn-mode").classList.remove("dark-button");
+}
+function darkInfo() {
+  document.querySelector("header").classList.add("dark-header");
+  document.querySelector("body").classList.add("dark-bckgnd");
+  document.querySelector(".btn-mode").classList.add("dark-button");
+  document.querySelector(".back-btn").classList.add("dark-button");
   document.querySelectorAll(".cardInfo span.border").forEach((item) => {
-    item.classList.toggle("dark-header");
+    item.classList.add("dark-header");
+  });
+}
+function lightInfo() {
+  document.querySelector("header").classList.remove("dark-header");
+  document.querySelector("body").classList.remove("dark-bckgnd");
+  document.querySelector(".btn-mode").classList.remove("dark-button");
+  document.querySelector(".back-btn").classList.remove("dark-button");
+  document.querySelectorAll(".cardInfo span.border").forEach((item) => {
+    item.classList.remove("dark-header");
   });
 }
