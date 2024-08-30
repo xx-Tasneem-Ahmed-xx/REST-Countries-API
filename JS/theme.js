2bbbe38c4bccfd981ce3412532b44f478f3cdaec
Doha <dohaamed22@gmail.com>
2024-08-30 15:30:43 +0300

    dark theme icons ,single card

--- JS/theme.js
+++ JS/theme.js
@@ -6,8 +6,17 @@ function changeTheme() {
   document.querySelector("body").classList.toggle("dark-bckgnd");
 
   document.querySelector("select").classList.toggle("dark-header");
-  // document.querySelector("option").classList.toggle("dark-header");
   document.querySelector(".search").classList.toggle("dark-header");
   document.querySelector(".search-input").classList.toggle("dark-header");
   document.querySelector(".btn-mode").classList.toggle("dark-button");
+}
+
+function infoTheme() {
+  document.querySelector("header").classList.toggle("dark-header");
+  document.querySelector("body").classList.toggle("dark-bckgnd");
+  document.querySelector(".btn-mode").classList.toggle("dark-button");
+  document.querySelector(".back-btn").classList.toggle("dark-button");
+  document.querySelectorAll(".cardInfo span.border").forEach((item) => {
+    item.classList.toggle("dark-header");
+  });
 }
